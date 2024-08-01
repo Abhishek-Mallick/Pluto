59e6fb5c8f766bdd5476e53bccd0ca234ac2db9e
soumyabrata <ankansam@gmail.com>
2024-08-01 18:23:54 +0530

    converted from jsx to tsx

---
+++ my-app/src/app/components/Header.tsx
@@ -0,0 +1,48 @@
+"use client";
+import Link from 'next/link';
+import { UserButton, useClerk, UserResource } from '@clerk/nextjs';
+import React, { FC } from 'react';
+
+const Header: FC = () => {
+  const { user } = useClerk();
+
+  return (
+    <nav className='flex items-center justify-between px-6 py-4 mb-5 bg-blue-700'>
+      <div className='flex items-center'>
+        <Link href='/'>
+          <div className='text-lg font-bold text-white uppercase'>
+            Clerk App
+          </div>
+        </Link>
+      </div>
+      <div className='flex items-center text-white'>
+        {!user && (
+          <>
+            <Link
+              href='/sign-in'
+              className='text-gray-300 hover:text-white mr-4'
+            >
+              Sign In
+            </Link>
+            <Link
+              href='/sign-up'
+              className='text-gray-300 hover:text-white mr-4'
+            >
+              Sign Up
+            </Link>
+          </>
+        )}
+        {user && (
+          <Link href='/profile' className='text-gray-300 hover:text-white mr-4'>
+            Profile
+          </Link>
+        )}
+        <div className='ml-auto'>
+          <UserButton afterSwitchSessionUrl='/' />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
