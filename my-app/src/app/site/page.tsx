4b2d240027a40f493fffdeefd06b7d6fb9b514b8
Harshitm14 <harshitmania3@gmail.com>
2024-08-10 13:58:32 +0530

    feat: added landing page

--- my-app/src/app/site/page.tsx
+++ my-app/src/app/site/page.tsx
@@ -1,14 +1,33 @@
+import { Main, Section, Container } from "@/components/craft";
+import CTA from "@/components/landing-page/cta";
+import FAQ from "@/components/landing-page/faq";
+import FirstFeature from "@/components/landing-page/firstfeature";
+import Footer from "@/components/landing-page/footer";
+import SecondFeature from "@/components/landing-page/secondfeature";
+import Hero from "@/components/landing-page/hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <section className="h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col ">
+    // <>
+    //   <section className="h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col ">
      
-          <h1 className="text-9xl font-bold text-center md:text-[300px]">
-            Pluto
-          </h1>
-      </section>
-    </>
+    //       <h1 className="text-9xl font-bold text-center md:text-[300px]">
+    //         Pluto
+    //       </h1>
+    //   </section>
+    // </>
+    <Main>
+      <Section>
+        <Container>
+          <Hero />
+          <FirstFeature />
+          <SecondFeature />
+          <CTA />
+          <FAQ />
+          <Footer />
+        </Container>
+      </Section>
+    </Main>
   );
 }
--- my-app/src/providers/theme-provider.tsx
+++ my-app/src/providers/theme-provider.tsx
@@ -1,4 +1,13 @@
 // write the theme here
+"use client"
+
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
 
 // dark and light mode
 
